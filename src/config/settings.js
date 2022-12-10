91ee24e8e9fd51d0f3d55db4064cc4b73a350d6d
Arose Niazi <arose.niazi@yahoo.com>
2022-12-11 03:20:55 +0500

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,9 +1,6 @@
 export const TITLE = 'Chez Chef';
 export const fullTitle = 'Chez Chef - Chef Portal';
 export const defaultPassword = '123456';
-export const defaultCoordinatortPassword = defaultPassword;
-export const defaultTeacherPassword = defaultPassword;
-export const defaultStudentPassword = defaultPassword;
 export const rowsPerPageList = [25, 50, 100, 200];
 export const defaultPerPage = 25;
 export const BackendURL = 'https://chezchef.herokuapp.com/';
@@ -11,22 +8,15 @@ export const BackendURLAPI = 'https://api.chezchefs.com/api/';
 
 
 export const LoginImage = "/static/illustrations/Chef.gif";
-export const CoordinatorLoginImage = "/static/illustrations/coordinator.png";
-export const TeacherLoginImage = "/static/illustrations/teacher.png";
-export const StudentLoginImage = "/static/illustrations/student.png";
 export const ForgetPageImage = "/static/illustrations/forgot.png";
 export const ResetPageImage = ForgetPageImage;
 export const Image404 = "/static/illustrations/404.png";
 export const Image401 = "/static/illustrations/401.webp";
 export const DefaultAvatar = '/static/mock-images/avatars/islamicavatar.png';
 export const DefaultUploadedFileImage = '/static/icons/file.png';
-export const NotificationSound = '/static/sounds/notification.mp3';
 
 export const DefaultFood = '/static/illustrations/missingfood.jpg';
 
-
-export const NotificationReload = 1000 * 30;
-export const DataReload = NotificationReload;
 export const RailFenceSize = 3;
 
 export const allowedExtensions = ['png', 'jpg', 'jpeg', 'mp3', 'pdf', 'zip', 'rar', 'docx', 'doc', 'ppt', 'pptx', 'txt'];
